a22d3c25421e298957d9a04ad2facdb34c5d6281
iGEORGE <www.igeorge.www@gmail.com>
2022-08-15 15:12:53 +0100

    Timestamp Microservice

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
+
+// enable cors (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,13 +21,31 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp", function (req, res) {
+  const now = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  });
+});
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let date_string = req.params.date_string;
+  let userInput = new Date(date_string);
+
+  if(userInput == "Invalid Date") {
+    res.json({"error": "Invalid Date"})
+  } else {
+    res.json({
+      "unix": userInput.getTime(),
+      "utc":userInput.toUTCString()
+    });
+  }
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
